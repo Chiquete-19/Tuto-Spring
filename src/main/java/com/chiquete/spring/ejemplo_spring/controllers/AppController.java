9e760a0136e4cb4a1da4a8cd151c9feca518e006
Fernando Chiquete <l21170293@culiacan.tecn.mx>
2026-01-28 19:38:52 -0700

    @ModelAttribute

--- src/main/java/com/chiquete/spring/ejemplo_spring/controllers/AppController.java
+++ src/main/java/com/chiquete/spring/ejemplo_spring/controllers/AppController.java
@@ -1,9 +1,13 @@
 package com.chiquete.spring.ejemplo_spring.controllers;
 
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 import com.chiquete.spring.ejemplo_spring.models.DTO.Logica;
 import com.chiquete.spring.ejemplo_spring.models.Empleados;
@@ -21,5 +25,15 @@ public class AppController {
         m.addAttribute("Empleado", emp);
         return "info";
     }
-    
+    //Otra manera de enviar dato de un modelo a la vista 
+    @ModelAttribute("empleados")
+    public List<Empleados> obtenerEmpleados(){
+        return Arrays.asList(
+            new Empleados("Fernando","Chiquete","V","Jefe",15000.00),
+            new Empleados("Ana","Lopez","M","Auxiliar",8000.00),
+            new Empleados("Luis","Martinez","R","Gerente",20000.00)
+        );
+    }
 }
+
+
